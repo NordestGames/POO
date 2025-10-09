bce1c2ba44efd3e234a6bd10fd0df9afd22f04a3
NordestGames <anailton.af.af@gmail.com>
2025-10-09 10:36:25 -0300

    feat: gerenciar fila de banco

---
+++ classes/banco/CaixaBanco.java
@@ -0,0 +1,18 @@
+
+public class CaixaBanco{
+    private static int clientesAtendidos = 0;
+    private int numeroCaixa;
+
+    public CaixaBanco(int numeroCaixa){
+        this.numeroCaixa = numeroCaixa;
+    }
+
+    public void iniciaAtendimento(){
+        clientesAtendidos++;
+        System.out.println("Atendendo cliente " + clientesAtendidos + " no caixa " + numeroCaixa);
+    }
+
+    public static void imprimeClientes(){
+        System.out.println("Clentes atendidos: " + clientesAtendidos);
+    }
+}
