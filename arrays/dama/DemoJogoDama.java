7ce5c82ca1dbb38a56d07231bd65814c9dc6405f
NordestGames <anailton.af.af@gmail.com>
2025-10-15 18:34:29 -0300

    feat: dama usando arrays

---
+++ arrays/dama/DemoJogoDama.java
@@ -0,0 +1,8 @@
+package dama;
+
+public class DemoJogoDama {
+    public static void main(String[] args) {
+        JogoDeDamas jogo = new JogoDeDamas();
+        System.out.println(jogo);
+    }
+}
